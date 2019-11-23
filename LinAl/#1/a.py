da2fda4c73f58ac1b3a3b20a22e21cabd4db6fc0
sunshader <38595317+sunshader@users.noreply.github.com>
2019-11-24 00:23:51 +0300

    Update a.py
    Add floating numbers support

--- LinAl/#1/a.py
+++ LinAl/#1/a.py
@@ -1,118 +1,66 @@
 
+import math
+
 def readnums(f):
     line = f.readline()
-    array = [int(x) for x in line.split()]
+    array = [float(x) for x in line.split()]
     return array
 
-class matrix:
-    def read(self, f):
-        [self.n, self.m] = readnums(f)
+class vec:
+    def __init__(self, a):
+        self.x = a[0]
+        self.y = a[1]
+        self.z = a[2]
 
-        self.container = []
-        data = readnums(f)
-        for i in range(self.n):
-            self.container.append([])
-            for j in range(self.m):
-                self.container[i].append(data[self.m * i + j])
+    def length(self):
+        return math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
 
-    def write(self, f):
-        for i in range(self.n):
-            for j in range(self.m - 1):
-                f.write(str(self.container[i][j]) + ' ')
-            f.write(str(self.container[i][self.m - 1]) + '\n')
+    def normalize(self):
+        len = math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+        self.x /= len
+        self.y /= len
+        self.z /= len
+        return self
 
-    def transpose(self):
-        c = matrix()
-        c.container = []
-        c.n = self.m
-        c.m = self.n
+    def normalizing(self):
+        len = math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+        return vec([self.x / len, self.y / len, self.z / len])
 
-        for i in range(c.n):
-            c.container.append([])
-            for j in range(c.m):
-                c.container[i].append(self.container[j][i])
-        return c
 
-    def multiply(self, number):
-        c = matrix()
-        c.container = []
-        c.n = self.n
-        c.m = self.m
+    def cross(self, another):
+        return vec([self.y * another.z - self.z * another.y, self.z * another.x - self.x * another.z, self.x * another.y - self.y * another.x])
 
-        for i in range(c.n):
-            c.container.append([])
-            for j in range(c.m):
-                c.container[i].append(self.container[i][j] * number)
-        return c
+    def dot(self, another):
+        return self.x * another.x + self.y * another.y + self.z * another.z
+
+    def angle(self, another):
+        cosine = vec.dot(self, another) / (self.length() * another.length())
+        return math.acos(cosine) * (180.0 / math.pi)
 
     def __add__(self, another):
-        if self.n != another.n or self.m != another.m:
-            raise ValueError
-
-        c = matrix()
-        c.container = []
-        c.n = self.n
-        c.m = self.m
-
-        for i in range(c.n):
-            c.container.append([])
-            for j in range(c.m):
-                c.container[i].append(self.container[i][j] + another.container[i][j])
-        return c
+        return vec([self.x + another.x, self.y + another.y, self.z + another.z])
 
     def __sub__(self, another):
-        if self.n != another.n or self.m != another.m:
-            raise ValueError
+        return vec([self.x - another.x, self.y - another.y, self.z - another.z])
 
-        c = matrix()
-        c.container = []
-        c.n = self.n
-        c.m = self.m
-
-        for i in range(c.n):
-            c.container.append([])
-            for j in range(c.m):
-                c.container[i].append(self.container[i][j] - another.container[i][j])
-        return c
-
-    def __mul__(self, another):
-        if self.m != another.n:
-            raise ValueError
-
-        c = matrix()
-        c.container = []
-        c.n = self.n
-        c.m = another.m
-
-        for i in range(c.n):
-            c.container.append([])
-            for j in range(c.m):
-                sum = 0
-                for k in range(min(self.m, another.n)):
-                    sum += self.container[i][k] * another.container[k][j]
-                c.container[i].append(sum)
-        return c
+    def __str__(self):
+        return '{0} {1} {2}'.format(self.x, self.y, self.z)
 
 """ main """
-# reading part
 fin = open('input.txt', 'r')
-fout = open('output.txt', 'w')
 
-[alpha, beta] = readnums(fin)
+""" reading part """
+pos = vec(readnums(fin))
+v = vec(readnums(fin))
+m = vec(readnums(fin))
+omega = vec(readnums(fin))
 
-matrices = []
-for i in range(5):
-    m = matrix()
-    m.read(fin)
-    matrices.append(m)
+""" get board """
+b = pos - omega
+if b.x > 0:
+    print(1)
+else:
+    print(0)
 
-# calculate equatation
-try:
-    x = matrices[2] * (matrices[0].multiply(alpha) + matrices[1].multiply(beta).transpose()).transpose() * matrices[3] - matrices[4]
-    fout.write(str(1) + '\n')
-    fout.write('{0} {1}\n'.format(x.n, x.m))
-    x.write(fout)
-except ValueError:
-    fout.write(str(0) + '\n')
-fin.close()
-fout.close()
+
+print(vec.angle(m, vec([0, 0, 1])))
